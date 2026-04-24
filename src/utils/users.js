ff7681de17d39b8d42ec65d12b2beecbdc636a68
BelaIsBelaG <icarabalig@estudiante.uniajc.edu.co>
2026-04-23 19:57:51 -0500

    agregamos los datos mock de usuarios

---
+++ src/utils/users.js
@@ -0,0 +1,22 @@
+export const users = [
+  {
+    id: "1",
+    name: "Usuario Prueba FoodieApp",
+    email: "foodieapp@test.com",
+    password: "123456"
+  },
+
+  {
+    id: "2",
+    name: "Laura Gómez",
+    email: "laura@test.com",
+    password: "123456"
+  },
+
+  {
+    id: "3",
+    name: "Carlos Ramírez",
+    email: "carlos@test.com",
+    password: "123456"
+  }
+];
