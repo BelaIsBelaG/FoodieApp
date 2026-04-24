8fd96257460be08bee92db9fa0788a4522905db3
BelaIsBelaG <icarabalig@estudiante.uniajc.edu.co>
2026-04-23 19:43:20 -0500

    Actualización del app.js

--- App.js
+++ App.js
@@ -1,5 +1,24 @@
 import AppNavigator from "./src/navigation/AppNavigator"
 
+//Agregado: importamos el Provider del contexto de carrito y favoritos
+import { FoodProvider } from "./src/context/FoodContext"
+
+//Agregado: importamos el Provider del contexto de usuario (auth)
+import { AuthProvider } from "./src/context/AuthContext"
+
 export default function App() {
-  return <AppNavigator />
+  return (
+    /*
+    Agregado:
+    Ahora la app tiene 2 contextos globales:
+
+    - AuthProvider → maneja usuario (login / email)
+    - FoodProvider → maneja carrito y favoritos
+    */
+    <AuthProvider>
+      <FoodProvider>
+        <AppNavigator />
+      </FoodProvider>
+    </AuthProvider>
+  )
 }
